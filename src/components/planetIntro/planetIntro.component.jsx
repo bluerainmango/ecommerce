2c5ec5d20177436e6dd49f2c0d36df47c19cead5
Emily Yu <bluerainmango@gmail.com>
2021-04-09 00:37:11 -0700

    added animation to planetIntro

--- src/components/planetIntro/planetIntro.component.jsx
+++ src/components/planetIntro/planetIntro.component.jsx
@@ -4,10 +4,15 @@ import "./planetIntro.styles.scss";
 
 const PlanetIntro = ({ planet }) => {
   return (
-    <section className="planetIntro">
+    <section className="planetIntro glitch">
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      {/* <div className="glitch__item" /> */}
       <div className="planetIntro__content">
-        <h1>{planet.title}</h1>
-        <h2>The most technically advanced city in universe</h2>
+        <h1 className="">{planet.title}</h1>
+        {/* <h2>The most technically advanced city in universe</h2> */}
       </div>
     </section>
   );
