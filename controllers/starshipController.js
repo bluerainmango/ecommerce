589f536cfa6834337ff8a4e0eabb5646b180a12f
Emily Yu <bluerainmango@gmail.com>
2021-05-05 02:36:20 -0700

    backend: added get all starships API endpoint

--- controllers/starshipController.js
+++ controllers/starshipController.js
@@ -1,5 +1,12 @@
-exports.getAllStarships = (req, res, next) => {
+const Starship = require("../models/starshipModel");
+const catchAsync = require("../util/catchAsync");
+
+exports.getAllStarships = catchAsync(async (req, res, next) => {
+  const starships = await Starship.find();
+
   res.status(200).json({
     status: "success",
+    result: starships.length,
+    data: starships,
   });
-};
+});
