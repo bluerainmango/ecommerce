5f8996abc11c47f5a7deb791ff133288070eec27
Emily Yu <bluerainmango@gmail.com>
2021-06-16 19:33:46 -0700

    frontend: added lay image loading to collection page

--- client/src/components/box3d/box3d.component.jsx
+++ client/src/components/box3d/box3d.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Button from "../button/button.component";
 
@@ -6,6 +6,52 @@ import "./box3d.styles.scss";
 
 const Box3d = ({ props }) => {
   // console.log("🍋 box3d props:", props);
+
+  const [frontImgUrls, setFrontImgUrls] = useState("");
+  const imgRef = useRef();
+  // const [bgImgUrls, setbgImgUrls] = useState("");
+  // const bgImgRef = useRef();
+
+  useEffect(() => {
+    if (!props?.collectionThumb) return;
+    // if (!props?.collectionThumb || !props?.featureImage) return;
+
+    // const imgExtension = props.category === "planets" ? ".jpeg" : ".png";
+
+    setFrontImgUrls({
+      imgLow: `${
+        process.env.REACT_APP_API_BASE_URL
+      }/${props.collectionThumb.replace(".png", "_100px.png")}`,
+      imgHigh: `${process.env.REACT_APP_API_BASE_URL}/${props.collectionThumb}`,
+    });
+
+    // setbgImgUrls({
+    //   imgLow: `${
+    //     process.env.REACT_APP_API_BASE_URL
+    //   }/${props.featureImage.replace(imgExtension, `_100px${imgExtension}`)}`,
+    //   imgHigh: `${process.env.REACT_APP_API_BASE_URL}/${props.featureImage}`,
+    // });
+  }, [props.collectionThumb, props.featureImage, props.category]);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = frontImgUrls.imgHigh;
+
+    imageLoader.onload = () => {
+      imgRef.current.src = frontImgUrls.imgHigh;
+      imgRef.current.classList.remove("lazy-img");
+    };
+  }, [frontImgUrls.imgHigh]);
+
+  // useEffect(() => {
+  //   const bgImageLoader = new Image();
+  //   bgImageLoader.src = bgImgUrls.imgHigh;
+
+  //   bgImageLoader.onload = () => {
+  //     bgImgRef.current.style.backgroundImage = `url(${bgImgUrls.imgHigh})`;
+  //     bgImgRef.current.classList.remove("lazy-img");
+  //   };
+  // }, [bgImgUrls.imgHigh]);
 
   return (
     <div>
@@ -24,21 +70,31 @@ const Box3d = ({ props }) => {
             <p className="price">${props.price}</p>
             <div className="img-wrapper">
               <img
-                src={`${process.env.REACT_APP_API_BASE_URL}/${props.collectionThumb}`}
+                ref={imgRef}
+                src={frontImgUrls?.imgLow}
                 alt={`${props.title}-collection-thumb`}
+                className="lazy-img"
               />
             </div>
           </div>
 
           <div
+            // ref={bgImgRef}
             className="right"
             style={{
               backgroundImage: `linear-gradient(
-                0deg,
-                rgba(0, 0, 0, 0.219) 0%,
-                rgba(0, 0, 0, 0) 100%
-              ),url(${process.env.REACT_APP_API_BASE_URL}/${props.featureImage})`,
+                   0deg,
+                   rgba(0, 0, 0, 0.9) 0%,
+                   rgba(0, 0, 0, 0) 100%
+                 ),url(${process.env.REACT_APP_API_BASE_URL}/${props.featureImage})`,
             }}
+            // style={{
+            //   backgroundImage: `linear-gradient(
+            //     0deg,
+            //     rgba(0, 0, 0, 0.219) 0%,
+            //     rgba(0, 0, 0, 0) 100%
+            //   ),url(${bgImgUrls.imgLow})`,
+            // }}
           >
             {/* <h2>{props.title}</h2> */}
             <ul>
